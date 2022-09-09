6b040b5abac494714eca88e2a3ab39907f7d4877
Sourav Banerjee <rjsourav88@gmail.com>
2022-09-09 10:48:57 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,12 @@ const router = Router();
 
 
 router.get('/:name/:age', (req, res, next) => {
+    try {
+        const name = req.params.name;
+        const age = req.params.age;
+        res.status(200).json({"message": `Hello ${name}, you are ${age} years old`})
+    } catch(e) {
+        return res.status(500).json(e);
+    }
 })
 module.exports = router;
